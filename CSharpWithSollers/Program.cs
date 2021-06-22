fc8594dc3446355142e4fb34b032a4c89f822da1
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2021-06-21 17:37:41 -0700

    Add project files.

---
+++ CSharpWithSollers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpWithSollers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The dot net network framework");
+            Console.ReadKey();
+        }
+    } 
+}
